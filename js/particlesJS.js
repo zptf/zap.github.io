46728f56dd630e8163a3144f5b0f6d336feddf21
hellvax <z4p.l3c@gmail.com>
2024-11-22 10:32:41 +0000

    anoda uon

---
+++ js/particlesJS.js
@@ -0,0 +1,87 @@
+
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 800,
+            },
+        },
+        color: {
+            value: "#ffffff",
+        },
+        shape: {
+            type: "circle",
+        },
+        opacity: {
+            value: 0.8,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+            },
+        },
+        size: {
+            value: 5,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 2,
+                size_min: 1,
+                sync: false,
+            },
+        },
+        line_linked: {
+            enable: false,
+        },
+        move: {
+            enable: true,
+            speed: 1,
+            direction: "bottom",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+        },
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: false,
+            },
+            onclick: {
+                enable: false,
+            },
+            resize: true,
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
+                    opacity: 1,
+                },
+            },
+            bubble: {
+                distance: 250,
+                size: 0,
+                duration: 2,
+                opacity: 0,
+            },
+            repulse: {
+                distance: 400,
+                duration: 0.4,
+            },
+            push: {
+                particles_nb: 4,
+            },
+            remove: {
+                particles_nb: 2,
+            },
+        },
+    },
+    retina_detect: true,
+});
---
+++ js/roleIconWrapper.js
@@ -0,0 +1,4 @@
+var roleIconWrapper = document.querySelector('.role-icon-wrapper');
+if (roleIconWrapper.innerHTML.trim() === '') {
+    roleIconWrapper.style.display = 'none';
+}
